3b45b6b8fc0b10f09d20f6b7a7499d37d69801ea
sfmth <sfmth0@gmail.com>
2022-09-21 13:33:11 +0430

    shift_add_mult/reduced beta to 4 bits

--- shift_add_mult/shift_add_mult.py
+++ shift_add_mult/shift_add_mult.py
@@ -18,42 +18,42 @@ matplotlib.use('wxAgg')
 # 239
 # 255
 def mult(beta, potential):
-    return int((beta/255)*potential)
+    return int((beta/8)*potential)
 
 def shift_add_mult(beta, potential):
     potential_2 = int(potential/2)
     potential_4 = int(potential/4)
     potential_8 = int(potential/8)
     
-    if ( 15 > beta >= 0 ):
+    if (beta == 0):
         return 0
     
-    elif (47 > beta >= 15):
+    elif (beta == 1):
         return potential_8
     
-    elif (79 > beta >= 47):
+    elif (beta == 2):
         return potential_4
     
-    elif (111 > beta >= 79):
+    elif (beta == 3):
         return potential_4 + potential_8
     
-    elif (143 > beta >= 111):
+    elif (beta == 4):
         return potential_2
     
-    elif (175 > beta >= 143):
+    elif (beta == 5):
         return potential_2 + potential_8
     
-    elif (207 > beta >= 175):
+    elif (beta == 6):
         return potential_2 + potential_4
     
-    elif (239 > beta >= 207):
+    elif (beta == 7):
         return potential_2 + potential_4 + potential_8
     
-    elif (255 >= beta >= 239):
+    elif (beta == 8):
         return potential
     
 error = []
-for i in range(256):
+for i in range(9):
     for j in range(256):
         error_i_j = shift_add_mult(i,j) - mult(i,j)
         error.append(error_i_j)
@@ -63,12 +63,12 @@ print(max(error))
 U = 122
 beta = []
 ans = []
-for i in range(256):
+for i in range(9):
     beta.append(i)
     ans.append(shift_add_mult(i,U))
     
 fig, ax = plt.subplots()
-ax.plot(beta, ans)
+ax.step(beta, ans, where='mid')
 ax.grid()
 ax.set(xlabel='beta', ylabel='shift-add multiplication',
        title='shift-add multiplication for U=122')
