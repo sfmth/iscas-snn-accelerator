40b179ddc77f520e03ae42551199dfd84af305a5
sfmth <sfmth0@gmail.com>
2022-10-10 22:36:43 +0330

    progress in neuron_selector

--- verilog-workbench/src/neuron_selector.v
+++ verilog-workbench/src/neuron_selector.v
@@ -7,60 +7,78 @@
 module neuron_selector(
     // generated potential by neurons:
     // output potential selector
-    input [8191:0] potential_out_all,
-    output [127:0] potential_out_16n,
-    input [5:0] cntrl_potential_out_sel,
+    input wire [8191:0] potential_out_all,
+    output wire [127:0] potential_out_16n,
+    input wire [5:0] cntrl_potential_out_sel,
 
     // accumulator input spk selector
-    output [2047:0] spk_out,
-    input [1023:0] in_spk,
-    input [1:0] processed_spk,
+    output wire [2047:0] spk_out,
+    input wire [1023:0] in_spk,
+    input wire [1:0] processed_spk,
 
-    // 
-    // input potential 
+    // existing potential from the previous time step:
+    // input potential
+    input wire [127:0] potential_in_16n,
+    output wire [8191:0] potential_in_all,
+    output wire [1023:0] potential_in_ien_all,
 
-
-
-
-
-
-
-    //potential read
-    input wire [127:0] potential_read_sram,
-    output wire [8:0] potential_read_sram_addr,
-
-    output wire [127:0] potential_read_out,
-
-    input wire [8:0] cntrl_potential_read_addr,
-
-    //beta read
-    input wire [63:0] beta_read_sram,
-    output wire [8:0] beta_read_sram_addr,
-
-    output wire [63:0] beta_read_out,
-
-    input wire [8:0] cntrl_beta_read_addr,
-
-    // potential write
-    output wire [127:0] potential_write_sram,
-    output wire [8:0] potential_write_sram_addr,
-
-    input wire [127:0] potential_write_in,
-
-    input wire [8:0] cntrl_potential_write_addr
+    input wire [5:0] cntrl_potential_in_sel,
+    input wire cntrl_potential_in_ien
     );
 
-    //potential read
-    assign potential_read_out = potential_read_sram;
-    assign potential_read_sram_addr = cntrl_potential_read_addr;
+    // potential out selector
+    reg [127:0] potential_out [0:63];
 
-    //beta read
-    assign beta_read_out = beta_read_sram;
-    assign beta_read_sram_addr = cntrl_beta_read_addr;
+    genvar i;
+    generate
+        for (i=0;i<64;i=i+1) begin
+            assign potential_out[i] = potential_out_all[(i*128)+127:i*128];
+        end
+    endgenerate
 
-    //potential write
-    assign potential_write_sram = potential_write_in;
-    assign potential_write_sram_addr = cntrl_potential_write_addr;
+    assign potential_out_16n = potential_out[cntrl_potential_out_sel];
+
+    // potential in selector
+
+    genvar j;
+    generate
+        for (j=0;j<64;j=j+1) begin
+            assign potential_in_all[(j*128)+127:j*128] = (cntrl_potential_in_sel == j) ?
+                potential_in_16n : 128'bx;
+        end
+    endgenerate
+
+    genvar k;
+    generate
+        for (k=0;k<64;k=k+1) begin
+            assign potential_in_ien_all[(k*16)+15:k*16] = (cntrl_potential_in_sel == k) ?
+                16'b1111111111111111 : 16'b0;
+        end
+    endgenerate
+
+
+
+
+    /* genvar i; */
+    /* generate */
+    /*     for (i=0;i<64;i=i+1) begin */
+    /*         if (cntrl_potential_out_sel == i) */
+    /*             assign potential_out_16n = potential_out_all[(i*128)+127:i*128]; */
+    /*     end */
+    /* endgenerate */
+
+
+
+
+
+    /* int i; */
+    /* always @(*) begin */
+    /*     for (i=0;i<64;i=i+1) begin */
+    /*         if (cntrl_potential_out_sel == i) */
+    /*             potential_out_16n = potential_out_all[i*128+:i*128]; */
+    /*     end */
+    /* end */
+
 
     `ifdef COCOTB_SIM
     initial begin
