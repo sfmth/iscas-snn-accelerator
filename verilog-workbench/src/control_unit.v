8d03f5667942b9c61bd1d62d97273633169eb336
sfmth <sfmth0@gmail.com>
2022-10-14 23:42:58 +0330

    cntrl unit progress

--- verilog-workbench/src/control_unit.v
+++ verilog-workbench/src/control_unit.v
@@ -52,22 +52,63 @@ module control_unit (
     reg [3:0] state;
     reg [7:0] time_step;
     reg [1:0] layer;
+    reg [9:0] cnt_512;
     always @(posedge clk) begin
         case (state)
             `STATE_INIT: begin
 
             end
+            `STATE_INPUT_0: begin
+
+            end
+            `STATE_PRE_INPUT: begin
+
+            end
             `STATE_INPUT: begin
 
             end
+            `STATE_PRE_HIDDEN: begin
+                cnt_512 <= 0;
+                cntrl_ac_oen <= 0;
+                // update addr
+                w_read_sram_addr  <= 1;
+                cntrl_ac_spk_read_addr <= 64;
+                cntrl_spkblty_read_addr <= 64;
+                cntrl_in_spk_read_addr <= 64;
+                cntrl_potential_read_addr <= 64;
+                cntrl_beta_read_addr <= 64;
+                cntrl_potential_write_addr <= 0;
+                cntrl_spkblty_write_addr <= 0;
+                cntrl_spk_write_addr <= 0;
+            end
             `STATE_HIDDEN: begin
-                
+                cntrl_ac_reset <= 0;
+                cnt_512 <= cnt_512 + 1;
+                // 512 cycles
+                if (cntrl_spk_write_addr > 0) begin
+                    w_read_sram_addr <= w_read_sram_addr + 1;
+                    cntrl_ac_spk_read_addr <= cntrl_ac_spk_read_addr + 1;
+                end
+                // 64 cycles
+                if (cnt_512 < 63) begin
+                cntrl_spk_write_addr <= cntrl_spk_write_addr + 1;
+                end else begin
+                    cntrl_spk_write_we <= 0;
+                end
+                // finish
+                if (cnt_512 == 511) begin
+                    cntrl_ac_oen <= 1;
+                    cntrl_ac_reset <= 1;
+                    state <= `STATE_PRE_OUTPUT;
+                end
+            end
+            `STATE_PRE_OUTPUT: begin
 
             end
             `STATE_OUTPUT: begin
 
             end
-            default:
+            default: state <= `STATE_INIT;
         endcase
     end
 
